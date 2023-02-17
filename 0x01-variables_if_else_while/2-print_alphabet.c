cdad7ecd6a25f75039a0c6653d10bb1302ead9af
BABYUNGOWETHU <122895135+BABYUNGOWETHU@users.noreply.github.com>
2023-02-17 18:58:14 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,36 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- *main -> assign a random number to the variable n each time it is executed
- *and print the last digit of the number stored in the variable n
- *Return: Always 0 (Success)
+ * main - Entry point
+ * Description: prints the alphabet in lowercase\n
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int n;
-	int x;
+	int ch;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	x = n % 10;
-	printf("Last digit of %d is %d ", n, x);
-	if (x > 5)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		printf("and is greater than 5");
+		putchar(ch);
 	}
-	if (x == 0)
-	{
-		printf("and is 0");
-	}
-	if (x < 6 && x != 0)
-	{
-		printf("and is less than 6 and not 0");
-	}
-
-	printf("\n");
-
-	return (0);
+	putchar('\n');
+return (0);
 }
