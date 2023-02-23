0e9b2e0991d4428f442caa0c19d216ed43fa8055
BABYUNGOWETHU <122895135+BABYUNGOWETHU@users.noreply.github.com>
2023-02-23 19:59:31 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,21 @@
-
 #include "main.h"
 
 /**
- * print_triangle - check for a digit
- * @size : integer type
- * Return:void
+ * print_triangle - print triangle of variable length
+ *
+ * @size: triangle height
  */
-
 void print_triangle(int size)
 {
-	int i = 1, ii;
+	int i, m;
 
-	while (i <= size && size > 0)
+	m = size;
+	if (size <= 0)
+		_putchar('\n');
+	while (size > 0)
 	{
-		ii = 0;
-		while (ii < size - i)
-		{
-			_putchar(' ');
-			ii++;
-		}
-		ii = 0;
-		while (ii < i)
-		{
-			_putchar('#');
-			ii++;
-		}
-
+		for (i = 0; i < m; i++)
+			_putchar((i < size - 1) ? ' ' : '#');
 		_putchar('\n');
-		i++;
+		size--;
 	}
-	if (i == 1)
-		_putchar('\n');
